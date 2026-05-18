66a3e7660de734b84e4bebbb3d794b3e139b0832
Shubham Sharma <shubhamappdev1@gmail.com>
2026-05-18 17:14:41 +0530

    Add home screen

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import "../styles/Home.css";
+import HeroImage from "../assets/heroImg.png"
 const HomePage = () => {
   return (
-    <h1>HomePage</h1>
+    <div className="main">
+      <div className="heading">
+        <h1>Shubham Sharma</h1>
+      </div>
+      <div className="container">
+        <ul className='list-1'>
+          <li>Blogs</li>
+          <li>Work</li>
+          <li>About</li>
+        </ul>
+        <img src={HeroImage} alt="admin looking at the camera in thinking position" className='hero-img'/>
+        <ul className='list-2'>
+          <li>Skills</li>
+          <li>Resume</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
