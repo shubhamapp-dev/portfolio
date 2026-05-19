38fa64c6ab8f854f53e97ca1097def93547cb1f6
Shubham Sharma <shubhamappdev1@gmail.com>
2026-05-19 12:17:05 +0530

    Add contact and skill page

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from '../componets/Header'
+import "../styles/Contact.css"
+import { Link } from 'react-router-dom'
 
 const ContactPage = () => {
   return (
     <>
       <Header name="Contact"/>
-      <div>ContactPage</div>
+      <div className="contact">
+        <Link to="https://linkedin.com/in/shubhamwork2026" target='_blank'><p>LinkedIn : @shubhamwork2026</p></Link>
+        <Link to="https://x.com/perrythehacker" target='_blank'><p>Twitter/X : @perrythehacker</p></Link>
+        <a href="mailto:shubhamshwork@outlook.com"><p>Email : shubhamshwork@outlook.com</p></a>
+        <a href="tel:+917901722460"><p>Phone : (+91) 7901722460</p></a>
+      </div>
     </>
   )
 }
--- frontend/src/pages/SkillPage.jsx
+++ frontend/src/pages/SkillPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Header from '../componets/Header'
+import "../styles/Skill.css"
 
 const SkillPage = () => {
   return (
     <>
       <Header name="Skills"/>
-      <div>SkillPage</div>
+      <div className="skill">
+        <p>Web Development : React, REST API, Express, NodeJS</p>
+        <p>Programming Language: Python, JavaScript, C++</p>
+        <p>Others: Unix, Git, Web Application Vulnerabilities</p>
+      </div>
     </>
   )
 }
